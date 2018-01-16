28dbfef3ad5576bbad9a96664517f025340798f0
Silvio Angelo <silvioangels@gmail.com>
2018-01-16 13:15:38 -0200

    Aula 7 -  Precisamos alimentar esse sistema - Video - Validando o Form

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,9 +1,21 @@
-angular.module('alurapic').controller('FotoController',function($scope) {
+angular.module('alurapic').controller('FotoController',function($scope, $http) {
 
-    $scope.foto = [];
+    $scope.foto = {};
+    $scope.mensagem = '';
+    //$scope.exibe = true;
 
     $scope.submeter = function () {
-          console.log($scope.foto);
+          if($scope.formulario.$valid){
+            $http.post('V1/fotos', $scope.foto)
+            .success(function(){
+              $scope.foto = {};
+              $scope.mensagem = 'Foto incluida com Sucesso';
+            })
+            .error(function(erro){
+              console.log(erro);
+              $scope.mensagem = 'Não foi possivel incluir a foto';
+            });
+          }
     };
 
 });
