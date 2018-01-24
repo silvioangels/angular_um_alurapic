b790526da9d4abc179f4fdfd1e7f94f72b3e96b6
Silvio Angelo <silvioangels@gmail.com>
2018-01-24 14:20:51 -0200

    Aula 10 -  E se o nosso back-end mudasse? - Video

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,42 +1,39 @@
-angular.module('alurapic').controller('FotoController',function($scope, $http, $routeParams) {
+angular.module('alurapic').controller('FotoController',function($scope, recursoFoto, $routeParams) {
 
     $scope.foto = {};
     $scope.mensagem = '';
-    //$scope.exibe = true;
 
     if($routeParams.fotoId){
-      $http.get('V1/fotos/'+$routeParams.fotoId)
-      .success(function(foto){
+
+      recursoFoto.get({fotoId : $routeParams.fotoId}, function(foto){
         $scope.foto = foto;
-      })
-      .error(function(erro){
+      }, function(){
         console.log(erro);
         $scope.mensagem = "Não foi possível obter a foto";
-      })
-      ;
+      });
+
     }
 
     $scope.submeter = function () {
+
           if($scope.foto._id){
-            $http.put('V1/fotos/'+$scope.foto._id, $scope.foto)
-            .success(function(){
+
+            recursoFoto.update({fotoId: $scope.foto._id}, $scope.foto, function(){
               $scope.mensagem = "A foto "+$scope.foto.titulo + ' foi alterada com sucesso.';
-            })
-            .error(function(erro){
+            }, function(erro){
               console.log(erro);
               $scope.mensagem = "Não foi possível alterar a foto "+$scope.foto.titulo;
-            })
-            ;
+            });
+
           }else{
 
             if($scope.formulario.$valid){
-              $http.post('V1/fotos', $scope.foto)
-              .success(function(){
+
+              recursoFoto.save($scope.foto, function(){
                 $scope.foto = {};
                 $scope.mensagem = 'Foto incluida com Sucesso';
                 $scope.formulario.$setPristine();
-              })
-              .error(function(erro){
+              }, function(erro){
                 console.log(erro);
                 $scope.mensagem = 'Não foi possivel incluir a foto';
               });
